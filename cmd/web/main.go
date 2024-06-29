126f55a8dbfd119103cdfea23cf97e42314ea005
mashuu-redox <mashuu-redox@proton.me>
2024-06-29 09:48:03 +0000

    add models package to main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,51 +7,48 @@ import (
 	"net/http"
 	"os"
 
+	"mashuu-redox/snippetbox/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Add a snippets field to the application struct. This will allow us to
+// make the SnippetModel object available to our handlers.
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	
-	// Define a new command-line flag for the MySQL DSN string.
+
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source new")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// To keep the main() function tidy I've put the code for creating a connection
-	// pool into the separate openDB() function below, We pass openDB() the DSN
-	// from the command-line flag.
 	db, err := openDB(*dsn)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
-	// We also defer a call to db.Close(), so that the connection pool is closed
-	// before the main() function exits.
 	defer db.Close()
 
+	// Initialize a models.SnippetModel instance containing the connection pool
+	// and add it to the application dependencies.
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", "addr", *addr)
 
-	// Because the err variable is now already declared in the code above, we need
-	// to use the assignment operator = here, instead of the := 'declare and assign'
-	// operator.
 	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
 
-// The openDB() function wraps sql.Open() and returns a sql.DB connection pool.
-// for a given DSN.
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
